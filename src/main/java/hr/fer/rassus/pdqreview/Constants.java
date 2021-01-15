6c2c96b2625d8938ec1bf333decd18f595f8ffdb
jezicKarlo <karlo.jezic@gmail.com>
2021-01-15 12:55:34 +0100

    add main and constants

---
+++ src/main/java/hr/fer/rassus/pdqreview/Constants.java
@@ -0,0 +1,26 @@
+package hr.fer.rassus.pdqreview;
+
+public class Constants {
+
+    private Constants() {
+    }
+
+    static final int NUMBER_OF_SERVERS = 1;
+
+    static final double a = 0.2;
+    static final double b = 0.3;
+    static final double c = 0.5;
+    static final double d = 0.3;
+    static final double e = 0.7;
+    static final double f = 0.5;
+    static final double g = 0.1;
+    static final double h = 0.3;
+
+    static final double S1 = 0.003;
+    static final double S2 = 0.001;
+    static final double S3 = 0.01;
+    static final double S4 = 0.04;
+    static final double S5 = 0.1;
+    static final double S6 = 0.13;
+    static final double S7 = 0.15;
+}
---
+++ src/main/java/hr/fer/rassus/pdqreview/Review.java
@@ -0,0 +1,10 @@
+package hr.fer.rassus.pdqreview;
+
+import com.perfdynamics.pdq.PDQ;
+
+public class Review {
+
+    public static void main(String[] args) {
+        PDQ pdq = new PDQ();
+    }
+}
